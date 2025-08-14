4b4819809b86ce6ca4cb2b89b710257419e62877
AmirHossein <amirhossein1mirzaei79@gmail.com>
2025-08-14 06:44:48 -0700

    fix tests

--- lib/features/country/data/source/country_data_source.dart
+++ lib/features/country/data/source/country_data_source.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
+
 import 'package:dio/dio.dart';
-import '../../../../core/error/failure.dart';
 import '../models/country.dart';
 
 abstract class ICountryRemoteDataSource {
@@ -14,19 +14,15 @@ class CountryRemoteDataSource implements ICountryRemoteDataSource {
 
   @override
   Future<List<Country>> getCountries() async {
-    try {
-      final response = await httpClient.get('https://raw.githubusercontent.com/PouriaMoradi021/countries-api/refs/heads/main/countries.json');
 
-      if (response.statusCode == 200) {
-        final List<dynamic> jsonList = jsonDecode(response.data);
-        return jsonList.map((e) => Country.fromJson(e)).toList();
-      } else {
-        throw Failure.server('خطای سرور: ${response.statusCode}');
-      }
-    } on DioException catch (e) {
-      throw Failure.network('اینترنت وصل نیست یا ارتباط برقرار نمی‌شود');
-    } catch (e) {
-      throw Failure.cache('خطای کش یا پردازش داده‌ها');
+    final response = await httpClient.get('https://raw.githubusercontent.com/PouriaMoradi021/countries-api/refs/heads/main/countries.json');
+
+    if (response.statusCode == 200) {
+
+      final List<dynamic> jsonList = jsonDecode(response.data);
+      return jsonList.map((e) => Country.fromJson(e)).toList();
+    } else {
+      throw Exception('Failed to load countries');
     }
   }
 }
