1feef210d365c5774db3640784a4fd47a38c17da
AmirHossein <amirhossein1mirzaei79@gmail.com>
2025-08-14 00:18:31 -0700

    feat: add CountryModel with Freezed implement CountryRemoteDataSource using Dio create CountryRepository with ValueNotifier

---
+++ lib/features/country/data/rep/country_repository.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:dio/dio.dart';
+
+import 'package:flutter_hiring_test_djy/features/country/data/models/country.dart';
+
+import '../source/country_data_source.dart';
+
+
+final countryRepository = CountryRepository(CountryRemoteDataSource(Dio()));
+
+abstract class ICountryRepository {
+  Future<List<Country>> getCountries();
+}
+
+class CountryRepository implements ICountryRepository {
+  static final ValueNotifier<List<Country>?> countriesNotifier =
+  ValueNotifier(null);
+
+  final CountryRemoteDataSource dataSource;
+
+  CountryRepository(this.dataSource);
+
+  @override
+  Future<List<Country>> getCountries() async {
+    final List<Country> countries = await dataSource.getCountries();
+    countriesNotifier.value = countries;
+    return countries;
+  }
+}
+
+
---
+++ lib/features/country/data/source/country_data_source.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import '../models/country.dart';
+
+abstract class ICountryRemoteDataSource {
+  Future<List<Country>> getCountries();
+}
+
+class CountryRemoteDataSource implements ICountryRemoteDataSource {
+  final Dio httpClient;
+
+  CountryRemoteDataSource(this.httpClient);
+
+  @override
+  Future<List<Country>> getCountries() async {
+
+    final response = await httpClient.get('https://raw.githubusercontent.com/PouriaMoradi021/countries-api/refs/heads/main/countries.json');
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonList = response.data;
+      return jsonList.map((e) => Country.fromJson(e)).toList();
+    } else {
+      throw Exception('Failed to load countries');
+    }
+  }
+}
